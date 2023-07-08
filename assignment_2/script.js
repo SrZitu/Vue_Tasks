971a48f5178f96378a00a3e163bbc593843bdc6f
SrZitu <zitu094@gmail.com>
2023-07-08 15:26:13 +0600

    updated

--- assignment_2/script.js
+++ assignment_2/script.js
@@ -17,7 +17,7 @@ const arr = [10, 20, 30, 40];
 function destructureExample(obj, arr) {
   const { name, age } = obj;
   const [index0, , index2] = arr;
-  return `name:${name} and age:${age}`;
+  return {name,age}
 }
 
 const result = destructureExample(obj, arr);
