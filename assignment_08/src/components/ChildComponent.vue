7e0c19ce2a0b1d5013e994f0dc80ad5fc985dce8
SrZitu <zitu094@gmail.com>
2023-08-20 15:54:10 +0600

    assignment_09

---
+++ assignment_08/src/components/ChildComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps,defineEmits } from 'vue';
+const props = defineProps({
+  message: String
+});
+
+defineEmits(['messageEmitted']);
+
+const MessageToParent="Hello from the ChildComponent";
+
+
+</script>
+
+<template>
+  <div class="bg-white border border-gray-300 p-4 rounded-lg shadow-md mt-5">
+    <p class="text-lg font-semibold">Child Component</p>
+    <h2 class="text-gray-600">{{ message }}</h2>
+    <button @click="$emit('messageEmitted',MessageToParent)" class="bg-green-500 hover:bg-blue-600 text-white px-2 py-2 mt-3">Emit Message</button>
+  </div>
+</template>
+
+
---
+++ assignment_08/src/components/ParentComponent.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+import ChildComponent from './ChildComponent.vue';
+
+const msg = "This is message from parent component";
+const emmitmsgShow = ref('');
+
+const handleChilMessage = (emittedMsg) => {
+  emmitmsgShow.value = emittedMsg;
+ 
+}
+</script>
+
+<template>
+  <div class="card lg:card-side bg-base-100 shadow-xl flex m-10 bg-gray-200 ">
+    <figure><img
+        src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+        alt="Album" /></figure>
+    <div class="card-body ml-10 mt-5">
+      <h2 class="card-title text-green-600 text-3xl">This Is Parent Component</h2>
+
+      <ChildComponent :message="msg" @messageEmitted="handleChilMessage" />
+
+      <div class="bg-white border border-gray-300 p-4 rounded-lg shadow-md my-5">
+        <h2 class="text-2xl text-blue-800">Message In Parent Component From Child Component</h2>
+        <p class="mt-5 text-purple-500 capitalize">Displaying emmited msg:{{ emmitmsgShow }}</p>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
